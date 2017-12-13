0736c6e517a8322ba760a074c773fc091d1b5af7
lsmgeb89 <lsmgeb89@gmail.com>
2017-12-13 14:08:42 -0600

    Initial version of minimal project

---
+++ lc/main.cc
@@ -0,0 +1,11 @@
+#include "sol.h"
+
+int main(void) {
+  Solution sol;
+  auto start = std::chrono::high_resolution_clock::now();
+  auto res = sol.Algo();
+  auto finish = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> elapsed = finish - start;
+  cout << "\nTime elapsed = " << elapsed.count() * 1000 << " ms\n";
+  return 0;
+}
---
+++ lc/sol.h
@@ -0,0 +1,8 @@
+#include "../lc_dbg/lc_dbg.h"
+
+class Solution {
+ public:
+  int Algo() {
+    return 0;
+  }
+};
